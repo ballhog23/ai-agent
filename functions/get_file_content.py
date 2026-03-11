2af8a1a2b20dbec4e805cc84eac613c9ff8ed819
caleb <calebpirkle2@gmail.com>
2026-03-11 06:26:46 -0500

    read file content and tests

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,9 +1,29 @@
 import os
+from config import MAX_CHARS
 
-def get_file_content(working_directory: str, directory: str = "."):
+def get_file_content(working_directory: str, file_path: str):
     try:
-        pass
+        working_dir_abs: str = os.path.abspath(working_directory)
+        target_file: str = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        # determine if target directory is within working dir abs path
+        valid_target_dir: bool = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+
+        if not valid_target_dir:
+            f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_file):
+            f'Error: File not found or is not a regular file: "{file_path}"'
         
-    
+        return read_file(target_file)
+
     except Exception as e:
-        return f"Error listing files: {e}"
+        return f"Error reading file content: {e}"
+
+def read_file(file_path: str) -> str:
+    with open(file_path, "r") as file:
+        file_contents = file.read(MAX_CHARS)
+
+        if file.read(1):
+            file_contents += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        
+    return file_contents
--- test_get_file_content.py
+++ test_get_file_content.py
@@ -3,19 +3,23 @@ from functions.get_file_content import get_file_content
 def test():
     tests = [
         {
-            "message": "Result for current directory:",
+            "message": "Result for 'calculator/lorem.txt' file:",
+            "data": get_file_content("calculator", "lorem.txt"),
+        },
+        {
+            "message": "Result for 'calculator/main.py' file:",
             "data": get_file_content("calculator", "main.py"),
         },
         {
-            "message": "Result for 'pkg' directory:",
+            "message": "Result for 'pkg/calculator.py' file:",
             "data": get_file_content("calculator", "pkg/calculator.py"),
         },
         {
-            "message": "Result for '/bin' directory:",
+            "message": "Result for '/bin/cat':",
             "data": get_file_content("calculator", "/bin/cat"),
         },
         {
-            "message": "Result for '../' directory:",
+            "message": "Result for 'pkg/does_not_exist.py' file:",
             "data": get_file_content("calculator", "pkg/does_not_exist.py"),
         },
     ]
